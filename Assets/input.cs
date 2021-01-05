caa2425809fa67eba24049ea972ba12efb94c8c2
Ishita Kamboj <ishitabargotakamboj@gmail.com>
2021-01-05 21:17:52 +0530

    first commit

---
+++ Assets/input.cs
@@ -0,0 +1,21 @@
+﻿ using System.Collections;
+ using System.Collections.Generic;
+ using UnityEngine;
+ using UnityEngine.UI;
+
+ public class input : MonoBehaviour
+ {
+    
+     public InputField n;
+    
+     public void onbc()
+     {
+        
+         Debug.Log( n.text );
+
+     }
+     void Start()
+     {
+         print("able to execute command");
+     }
+ }
